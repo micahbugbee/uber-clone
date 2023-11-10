7e39f1fd3858ab68a9070915d1afcd4ce0eeb4f5
Micah Bugbee <micahbugbee@hotmail.com>
2023-11-09 22:05:49 -0600

    Integrated google autocomplete search with redux and google navigation

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import MapScreen from "./screens/MapScreen";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // 1) Setup Redux
 
 export default function App() {
+  
+
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
